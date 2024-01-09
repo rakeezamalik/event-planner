78b4c8c5dc1dac1250c162ddabfddcc7e01907ba
Rakeeza <rakeeza.malik@and.digital>
2024-01-09 12:14:51 +0000

    Creating molecules for header and footer

---
+++ components/molecules/header/header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <button>burger menu icon which when open, opens a dropdown</button>
+        </li>
+        <li>
+          <span>Welcome</span>
+        </li>
+        <li>
+          <button>fake search icon</button>
+        </li>
+      </ul>
+    </header>
+  );
+}
