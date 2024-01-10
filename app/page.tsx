f5db999f8d42988647ced8cebbecf5bd4f063ccc
rakeezamal <156079572+rakeezamal@users.noreply.github.com>
2024-01-10 12:32:53 +0000

    Fixing semantic html

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import { Footer, Header } from "@/components/molecules";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
       <Header />
+      <main className="flex h-full flex-col items-center justify-between p-24"></main>
       <Footer />
-    </main>
+    </>
   );
 }
